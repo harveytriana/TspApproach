3054cfca54cc1de96285076c24161ed0deea046c
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2021-09-08 03:39:53 -0500

    Update main.go

--- Go/main.go
+++ Go/main.go
@@ -1,10 +1,14 @@
 /*
 TSP execise
-By: harveytriana@gmail.com
 
-go run main.go
+RUN
+$ go run main.go
 
-RESULT
+OUTPUT
+Nodes         : 13
+Iterations    : 479,001,600
+Nodules       : 1 2 3 4 5 6 7 8 9 10 11 12
+...
 Optimus route : 0 7,2,3,4,12,6,8,1,11,10,5,9 0
 Distance      :  7293
 Elapse Time   :  16.5590105s

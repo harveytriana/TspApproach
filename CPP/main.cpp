a8d2b3c13855af0ad15d6513bb910786c8466a4e
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2021-09-22 15:19:15 -0500

    Update main.cpp

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -10,11 +10,6 @@ Nodes         : 13
 Iterations    : 479,001,600
 Nodules       : 1 2 3 4 5 6 7 8 9 10 11 12
 ...
-Optimus route : 0 7 2 3 4 12 6 8 1 11 10 5 9 0
-Distance      : 7293
-Elapse time   : 8.348
-
-long
 Optimus route : 7 2 3 4 12 6 8 1 11 10 5 9
 Distance      : 7293
 Elapse time   : 8.213

c95dc08d031de00da158367483485a0dc3c4461a
Tedd Hansen <tedd@konge.net>
2021-10-01 19:41:24 +0200

    C#: Multidimensional to single-dimensional array: 19.5% (76.8%)

--- Csharp/TspInt32.cs
+++ Csharp/TspInt32.cs
@@ -20,24 +20,25 @@ using System.Text;
 
 class TspInt32
 {
-    public static void Run() {
+    public static void Run()
+    {
 
-    int[,] a = {
-            { 0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972 },
-            { 2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579 },
-            { 713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260 },
-            { 1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987 },
-            { 1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371 },
-            { 1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999 },
-            { 2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701 },
-            { 213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099 },
-            { 2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600 },
-            { 875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162 },
-            { 1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200 },
-            { 2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504 },
-            { 1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0 },
+        int[] a = {
+             0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972,
+             2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579,
+             713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260,
+             1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987,
+             1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371,
+             1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999,
+             2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701,
+             213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099,
+             2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600,
+             875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162,
+             1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200,
+             2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504,
+             1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0,
         };
-        
+
         Console.WriteLine("Traveling Salesman Problem Exact algorithm");
         new TspExactInt32().GetOptimusRoute(a);
 
@@ -48,7 +49,9 @@ class TspInt32
 
 class TspExactInt32
 {
-    int[,] _data;
+    const int DataWidth = 13;
+    const int DataLength = 13;
+    int[] _data;
     int _depot;
     int _nodes;
     int _nodulesCount;
@@ -61,9 +64,10 @@ class TspExactInt32
     StringBuilder _route;
     DateTime _now;
 
-    public void GetOptimusRoute(int[,] data, int depot = 0)
+    public void GetOptimusRoute(int[] data, int depot = 0)
     {
-        if((_nodes = data.GetLength(0)) > 13) {
+        if ((_nodes = DataLength) > 13)
+        {
             Console.WriteLine("The maximum number of nodes for Int32 is 13.");
             return;
         }
@@ -80,8 +84,10 @@ class TspExactInt32
         // arrangement of permutations
         _nodules = new int[_nodulesCount];
         int j = 0;
-        for (int i = 0; i < _nodes; i++) {
-            if (i != _depot) {
+        for (int i = 0; i < _nodes; i++)
+        {
+            if (i != _depot)
+            {
                 _nodules[j++] = i;
             }
         }
@@ -102,31 +108,38 @@ class TspExactInt32
 
     void GetRoute(int start, int end)
     {
-        if (start == end - 1) {
+        if (start == end - 1)
+        {
             // validate distance
             // 1. boundaries A..N, N..A
-            var sum = _data[_depot, _nodules[0]] +
-                   _data[_nodules[_nodulesCount - 1], _depot];
+            var sum = _data[_depot * DataWidth + _nodules[0]] +
+                   _data[_nodules[_nodulesCount - 1] * DataWidth + _depot];
             // 2. route
-            for (int i = 0; i < _nodulesCount - 1; i++) {
-                sum += _data[_nodules[i], _nodules[i + 1]];
+            for (int i = 0; i < _nodulesCount - 1; i++)
+            {
+                sum += _data[_nodules[i] * DataWidth + _nodules[i + 1]];
             }
             _permutation++;
-            if (_distance > sum) {// update minimun
+            if (_distance > sum)
+            {// update minimun
                 _distance = sum;
                 _route.Clear();
-                for (int i = 0; i < _nodulesCount; i++) {
+                for (int i = 0; i < _nodulesCount; i++)
+                {
                     _route.Append(_nodules[i]);
                     _route.Append(" ");
                 }
             }
-            if (_percentSize > 0 && _permutation % _percentSize == 0) {
+            if (_percentSize > 0 && _permutation % _percentSize == 0)
+            {
                 _percent += 1;
                 Console.WriteLine("Permutations: {0} % {1}", _percent, ElapseTime());
             }
         }
-        else {
-            for (var i = start; i < end; i++) {
+        else
+        {
+            for (var i = start; i < end; i++)
+            {
                 // swap
                 // Swap(start, i);
                 (_nodules[start], _nodules[i]) = (_nodules[i], _nodules[start]);
--- Csharp/main.cs
+++ Csharp/main.cs
@@ -22,24 +22,25 @@ using System.Text;
 
 class Program
 {
-    public static void Main() {
+    public static void Main()
+    {
         // Try to get a stable reading on every pass by maxing priority
         System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.RealTime;
 
-        long[,] a = {
-                { 0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972 },
-                { 2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579 },
-                { 713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260 },
-                { 1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987 },
-                { 1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371 },
-                { 1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999 },
-                { 2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701 },
-                { 213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099 },
-                { 2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600 },
-                { 875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162 },
-                { 1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200 },
-                { 2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504 },
-                { 1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0 },
+        long[] a = {
+                 0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972,
+                 2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579,
+                 713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260,
+                 1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987,
+                 1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371,
+                 1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999,
+                 2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701,
+                 213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099,
+                 2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600,
+                 875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162,
+                 1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200,
+                 2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504,
+                 1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0,
             };
         //long[,] a = {
         //    { 00, 10, 35, 30},
@@ -58,7 +59,9 @@ class Program
 
 class TspExact
 {
-    long[,] _data;
+    const int DataWidth = 13;
+    const int DataLength = 13;
+    long[] _data;
     long _depot;
     long _nodes;
     long _nodulesCount;
@@ -70,10 +73,11 @@ class TspExact
     long[] _nodules;
     StringBuilder _route;
 
-    public void GetOptimusRoute(long[,] data, long depot = 0) {
+    public void GetOptimusRoute(long[] data, long depot = 0)
+    {
         _data = data;
         _depot = depot;
-        _nodes = _data.GetLength(0);
+        _nodes =DataLength;
         _nodulesCount = _nodes - 1;
         _iterations = Factorial(_nodulesCount);
         _percentSize = _iterations / 100;
@@ -85,8 +89,10 @@ class TspExact
         // arrangement of permutations
         _nodules = new long[_nodulesCount];
         long j = 0;
-        for (long i = 0; i < _nodes; i++) {
-            if (i != _depot) {
+        for (long i = 0; i < _nodes; i++)
+        {
+            if (i != _depot)
+            {
                 _nodules[j++] = i;
             }
         }
@@ -103,31 +109,40 @@ class TspExact
         Console.WriteLine("Elapse Time   : {0} s", (DateTime.Now - now).TotalSeconds.ToString("N4"));
     }
 
-    void GetRoute(long start, long end) {
-        if (start == end - 1) {
+    void GetRoute(long start, long end)
+    {
+        if (start == end - 1)
+        {
             // validate distance
             // 1. boundaries A..N, N..A
-            var sum = _data[_depot, _nodules[0]] +
-                   _data[_nodules[_nodulesCount - 1], _depot];
+            var sum = _data[_depot * DataWidth + _nodules[0]] +
+                   _data[_nodules[_nodulesCount - 1] * DataWidth + _depot];
             // 2. route
-            for (long i = 0; i < _nodulesCount - 1; i++) {
-                sum += _data[_nodules[i], _nodules[i + 1]];
+            for (long i = 0; i < _nodulesCount - 1; i++)
+            {
+                sum += _data[_nodules[i] * DataWidth + _nodules[i + 1]];
             }
             _permutation++;
-            if (_distance > sum) {// update minimun
+            if (_distance > sum)
+            {// update minimun
                 _distance = sum;
                 _route.Clear();
-                for (long i = 0; i < _nodulesCount; i++) {
+                for (long i = 0; i < _nodulesCount; i++)
+                {
                     _route.Append(_nodules[i]);
                     _route.Append(" ");
                 }
             }
-            if (_percentSize > 0 && _permutation % _percentSize == 0) {
+            if (_percentSize > 0 && _permutation % _percentSize == 0)
+            {
                 _percent += 1;
                 Console.WriteLine("Permutations: {0} %", _percent);
             }
-        } else {
-            for (var i = start; i < end; i++) {
+        }
+        else
+        {
+            for (var i = start; i < end; i++)
+            {
                 // swap
                 (_nodules[start], _nodules[i]) = (_nodules[i], _nodules[start]);
                 // permute
@@ -138,7 +153,8 @@ class TspExact
         }
     }
 
-    long Factorial(long number) {
+    long Factorial(long number)
+    {
         if (number < 2)
             return 1;
         else
